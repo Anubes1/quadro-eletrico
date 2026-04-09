8b104d5c2417563d50975bd0520e4c0e1ffd3bf3
Anubes <gabrielrochafreitas7102804@gmail.com>
2026-04-08 21:16:02 -0300

    Primeiro commit

---
+++ sw.js
@@ -0,0 +1,58 @@
+// Service Worker para Ficha de Campo - Quadro Elétrico
+const CACHE_NAME = 'quadro-eletrico-v1';
+const urlsToCache = [
+  './html/quadro_eletrico.html',
+  './html/instrucoes.html',
+  './css/quadro-eletrico.css',
+  './icons/icon.svg',
+  './manifest.json'
+];
+
+// Install cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
+
+// Clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
